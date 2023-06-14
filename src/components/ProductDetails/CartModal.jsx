cf52a34223003a3a69575e8612f8a350a9f1311c
Yash Yadav <yashyadavgit@gmail.com>
2023-06-14 17:38:39 +0530

    cart-modal added

---
+++ src/components/ProductDetails/CartModal.jsx
@@ -0,0 +1,90 @@
+import addSign from "../../assests/Cart/plus-svgrepo-com.svg";
+import minusSign from "../../assests/Cart/minus-svgrepo-com.svg";
+import CancelSign from "../../assests/Cart/cancel.png";
+import "./CartModal.css";
+import { useState } from "react";
+
+const CartModal = ({ showCart, handleCartClick, data }) => {
+  const [counter, setCounter] = useState(1);
+  const [error, setError] = useState("");
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const handleCounterClick = (buttonClicked) => {
+    if (buttonClicked === "add") {
+      if (counter >= 6) {
+        setError("You can oly add 6 items");
+        return;
+      }
+      setCounter((prev) => prev + 1);
+    } else if (buttonClicked === "sub") {
+      if (counter === 1) {
+        setError("Atleast one item should be there in cart");
+        return;
+      }
+      setCounter((prev) => prev - 1);
+    }
+  };
+  return (
+    <>
+      {showCart ? (
+        <div className="modal-container">
+          <div className="dark-modal"></div>
+          <div className="modal-main">
+            <div className="modal-header">
+              <div className="modal-heading">
+                <span>Modal Heading</span>
+              </div>
+              <div
+                className="modal-cross-icon"
+                onClick={() => handleCartClick()}
+              >
+                X
+              </div>
+            </div>
+            <div className="modal-product-list-container">
+              <div className="modal-product-item">
+                <div className="modal-image">
+                  <img src={data.imageLeft} />
+                </div>
+                <div className="modal-sneaker-information">
+                  <div className="sneaker-information-heading">
+                    <div className="modal-brand-name">{data.brand}</div>
+                    <div className="item-cross-icon">X</div>
+                  </div>
+                  <div className="modal-shoe-name">{data.shoeName}</div>
+                  <div className="modal-shoe-size">Shoe Size: (UK):6</div>
+                  <div className="modal-price-calculation">
+                    <div className="modal-price">6,000</div>
+                    <div className="price-toggle-container">
+                      <button
+                        className="toggle-button"
+                        onClick={() => handleCounterClick("sub")}
+                      >
+                        <img src={minusSign} />
+                      </button>
+                      <div className="no-of-items">{counter}</div>
+                      <button
+                        className="toggle-button"
+                        onClick={() => handleCounterClick("add")}
+                      >
+                        <img src={addSign} />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer-section">
+              <div className="modal-subtotal-section">
+                <div className="modal-sub-total-heading">SUB TOTAL</div>
+                <div className="modal-sub-total-price">{totalPrice}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default CartModal;
--- src/pages/ProductDetails/ProuctDetails.jsx
+++ src/pages/ProductDetails/ProuctDetails.jsx
@@ -2,6 +2,8 @@ import { useLocation, useParams } from "react-router-dom";
 import "./ProductDetails.css";
 import { getPrice } from "../../utlis/common";
 import { useState } from "react";
+import Navbar from "../../components/Homepage/Navbar/Navbar";
+import CartModal from "../../components/ProductDetails/CartModal";
 
 const ProuctDetails = () => {
   const { state } = useLocation();
@@ -9,81 +11,101 @@ const ProuctDetails = () => {
   console.log(data);
   const [size, setSize] = useState(7);
   const [aboutSectionClicked, setAboutSectionClicked] = useState(false);
+  const [showCart, setShowCart] = useState(false);
+
+  const handleCartClick = () => {
+    setShowCart((prev) => !prev);
+    console.log("handle cart clicked");
+  };
 
   return (
-    <div className="product-details-caontainer">
-      <div className="product-details-images-conatiner">
-        <img src={data.imageLeft} />
-        <img src={data.imageRight} />
-        <img src={data.imageLeft} />
-        <img src={data.imageRight} />
+    <>
+      <div className={showCart ? "no-scroll" : ""}>
+        <Navbar />
       </div>
-
-      <div className="prouduct-details-information-container">
-        <div className="product-details-brand-name">{data.brand}</div>
-        <div className="product-details-sneaker-details">{data.shoeName}</div>
-        <div className="product-details-price">
-          &#8377; {getPrice(data.Price)}
+      <div
+        className={`product-details-caontainer ${showCart ? "no-scroll" : ""}`}
+      >
+        <div className="product-details-images-conatiner">
+          <img src={data.imageLeft} />
+          <img src={data.imageRight} />
+          <img src={data.imageLeft} />
+          <img src={data.imageRight} />
         </div>
-        <div className="size-container">
-          <div
-            className={`size-box ${size === 7 ? "selected" : null}`}
-            onClick={(e) => setSize(7)}
-          >
-            7
-          </div>
-          <div
-            className={`size-box ${size === 8 ? "selected" : null}`}
-            onClick={(e) => setSize(8)}
-          >
-            8
-          </div>
-          <div
-            className={`size-box ${size === 9 ? "selected" : null}`}
-            onClick={(e) => setSize(9)}
-          >
-            9
-          </div>
-          <div
-            className={`size-box ${size === 10 ? "selected" : null}`}
-            onClick={(e) => setSize(10)}
-          >
-            10
-          </div>
-          <div
-            className={`size-box ${size === 11 ? "selected" : null}`}
-            onClick={(e) => setSize(11)}
-          >
-            11
-          </div>
-          <div
-            className={`size-box ${size === 12 ? "selected" : null}`}
-            onClick={(e) => setSize(12)}
-          >
-            12
-          </div>
-        </div>
-        <div className="add-to-cart-button">ADD TO CART</div>
 
-        <div
-          className="about-section-container"
-          onClick={() => setAboutSectionClicked((prev) => !prev)}
-        >
-          <div className="about-product-heading">
-            <p>ABOUT PRODUCT</p>
-            <p>+</p>
+        <div className="prouduct-details-information-container">
+          <div className="product-details-brand-name">{data.brand}</div>
+          <div className="product-details-sneaker-details">{data.shoeName}</div>
+          <div className="product-details-price">
+            &#8377; {getPrice(data.Price)}
           </div>
-          {aboutSectionClicked ? (
-            <div className="product-about-list">
-              Manufacturer : Nike India Pvt. Ltd. Country of Origin : Vietnam
-              Imported By : Nike India Pvt. Ltd. Weight : 0.95 KG Generic Name :
-              SHOE Unit of Measurement : 1 Pair Marketed By : Superkicks India
-              Pvt. Ltd. Article Code : DV7183-100
+          <div className="size-container">
+            <div
+              className={`size-box ${size === 7 ? "selected" : null}`}
+              onClick={(e) => setSize(7)}
+            >
+              7
             </div>
-          ) : null}
+            <div
+              className={`size-box ${size === 8 ? "selected" : null}`}
+              onClick={(e) => setSize(8)}
+            >
+              8
+            </div>
+            <div
+              className={`size-box ${size === 9 ? "selected" : null}`}
+              onClick={(e) => setSize(9)}
+            >
+              9
+            </div>
+            <div
+              className={`size-box ${size === 10 ? "selected" : null}`}
+              onClick={(e) => setSize(10)}
+            >
+              10
+            </div>
+            <div
+              className={`size-box ${size === 11 ? "selected" : null}`}
+              onClick={(e) => setSize(11)}
+            >
+              11
+            </div>
+            <div
+              className={`size-box ${size === 12 ? "selected" : null}`}
+              onClick={(e) => setSize(12)}
+            >
+              12
+            </div>
+          </div>
+          <div className="add-to-cart-button" onClick={handleCartClick}>
+            ADD TO CART
+          </div>
+
+          <div
+            className="about-section-container"
+            onClick={() => setAboutSectionClicked((prev) => !prev)}
+          >
+            <div className="about-product-heading">
+              <p>ABOUT PRODUCT</p>
+              <p>+</p>
+            </div>
+            {aboutSectionClicked ? (
+              <div className="product-about-list">
+                Manufacturer : Nike India Pvt. Ltd. Country of Origin : Vietnam
+                Imported By : Nike India Pvt. Ltd. Weight : 0.95 KG Generic Name
+                : SHOE Unit of Measurement : 1 Pair Marketed By : Superkicks
+                India Pvt. Ltd. Article Code : DV7183-100
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
-    </div>
+      <CartModal
+        showCart={showCart}
+        data={data}
+        handleCartClick={handleCartClick}
+      />
+    </>
   );
 };
 
